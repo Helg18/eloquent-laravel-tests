934fa89054421c520cf8d14d34cea219701d9060
Henry Leon <Helg18@gmail.com>
2017-06-09 17:39:03 -0400

    Chaged app's name

--- config/app.php
+++ config/app.php
@@ -171,11 +171,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        Eloquent\Providers\AppServiceProvider::class,
+        Eloquent\Providers\AuthServiceProvider::class,
+        // Eloquent\Providers\BroadcastServiceProvider::class,
+        Eloquent\Providers\EventServiceProvider::class,
+        Eloquent\Providers\RouteServiceProvider::class,
 
     ],
 
